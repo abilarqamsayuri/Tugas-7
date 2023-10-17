04639f0b8508873ed8c148ae976977a933409ee0
abilarqamsayuri <144302495+abilarqamsayuri@users.noreply.github.com>
2023-10-17 09:54:05 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// membuat array
+var hewan = ['Gajah','Ikan','Harimau'];
+
+// menambahkan gagak ke dalam array hewan
+hewan.push('Gagak');
+
+// mengubah indeks 2 menjadi kucing
+hewan[2]='kucing'; 
+
+// menghapus indeks 0
+hewan.shift();
